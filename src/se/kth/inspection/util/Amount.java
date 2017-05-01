6666dfdcf9f199b674e0ad89e43887c1ad4bc348
William Åkerman <wakerman@kth.se>
2017-05-02 01:05:33 +0200

    Update Amount.java

--- src/se/kth/inspection/util/Amount.java
+++ src/se/kth/inspection/util/Amount.java
@@ -6,24 +6,24 @@ package se.kth.inspection.util;
  */
 public class Amount {
 	
-	   public final int amount;
+	public final int amount;
 
-	    /**
-	     * Creates a new instance, representing the specified amount.
-	     *
-	     * @param amount The amount represented by the newly created instance.
-	     */
-	    public Amount (int amount) {
-	        this.amount = amount;
-	    }
-	    
-		/**
-		 * The method to print the <code>Amount</code> object 
-		 * 
-		 */
-	    public String toString () {
-			StringBuilder amountMaker = new StringBuilder();
-			amountMaker.append(amount);
-			return amountMaker.toString();
-	    }
+	/**
+	 * Creates a new instance, representing the specified amount.
+	 *
+	 * @param amount The amount represented by the newly created instance.
+	 */
+	public Amount (int amount) {
+		this.amount = amount;
+	}
+	
+	 /**
+	  * The method to print the <code>Amount</code> object 
+	  * 
+	  */
+	 public String toString () {
+		 StringBuilder amountBuilder = new StringBuilder();
+		 amountBuilder.append(amount);
+		 return amountBuilder.toString();
+	 }
 }
