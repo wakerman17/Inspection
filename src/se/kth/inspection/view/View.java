6cdf1d90a8bbbe5cb07b1d9d9a464d672edeeef1
William Åkerman <wakerman@kth.se>
2017-05-02 01:24:52 +0200

    Update View.java

--- src/se/kth/inspection/view/View.java
+++ src/se/kth/inspection/view/View.java
@@ -3,11 +3,11 @@ package se.kth.inspection.view;
 import se.kth.inspection.controller.GarageDoorController;
 import se.kth.inspection.controller.SimultaniusDisplayAndGarageDoorController;
 import se.kth.inspection.util.Amount;
+import se.kth.inspection.util.CreditCardInformation;
 import se.kth.inspection.util.Inspect;
 import se.kth.inspection.util.Vehicle;
 import se.kth.inspection.controller.CostController;
 import se.kth.inspection.controller.PaymentController;
-import se.kth.inspection.model.CreditCardInformation;
 import se.kth.inspection.controller.InspectionController;
 import se.kth.inspection.controller.ResultController;
 
@@ -31,13 +31,13 @@ public class View {
 	private String holder = "William";
 	private String expireDate = "02/20";
 	private int cvc = 123;
-	private String result;
 	private CreditCardInformation creditCardInformation = new CreditCardInformation(pin, number, holder, expireDate, cvc);
+	private String result;
 	
 
 	public View(GarageDoorController garageDoorContr, SimultaniusDisplayAndGarageDoorController simuContr, 
-			    CostController costContr, PaymentController paymentContr, InspectionController inspectionContr,
-			    ResultController resultContr) {
+		    CostController costContr, PaymentController paymentContr, InspectionController inspectionContr,
+		    ResultController resultContr) {
 		this.garageDoorContr = garageDoorContr;
 		this.simuContr = simuContr;
 		this.costContr = costContr;
@@ -52,33 +52,43 @@ public class View {
 	public void sampleExecution () {
 		System.out.println("New inspection is made - new queue number and open door");
 		simuContr.newInspection();
-		status = "closed";
+		
+		status = "close";
 		System.out.println("Door closes");
 		garageDoorContr.activateDoor(status);
+		
 		Vehicle vehicle = new Vehicle(regNo);
+		System.out.println("Customer: What is your registration number? \n" + regNo);
 		cost = costContr.whatToPay(vehicle);
 		System.out.println("The cost is " + cost);
+		System.out.println("Customer: Print your creditCardInformation");
+		System.out.println(creditCardInformation);
+		System.out.println("Payment process is starting");
 		paymentContr.doPayment(creditCardInformation);
+		
 		Inspect toInspect = inspectionContr.whatToInspect(vehicle); 
-		System.out.println("You shold inspect: " + toInspect);
+		System.out.println("Inspector: \nYou shold inspect: " + toInspect);
+		
 		result = "pass";
 		resultContr.saveResult(result, vehicle);
 		toInspect = inspectionContr.whatToInspect(vehicle);
 		System.out.println("You shold inspect: " + toInspect);
+		
 		resultContr.saveResult(result, vehicle);
 		toInspect = inspectionContr.whatToInspect(vehicle);
-		System.out.println("You shold inspect: " + toInspect + "\n");
+		System.out.println("You shold inspect: " + toInspect);
+		
 		resultContr.saveResult(result, vehicle);
-		System.out.println("\n");
-		status = "opened";
+		System.out.println();
+		status = "open";
 		System.out.println("Door opens");
 		garageDoorContr.activateDoor(status);
-		status = "closed";
+		status = "close";
 		System.out.println("Door closes");
 		garageDoorContr.activateDoor(status);
+		
 		System.out.println("\nNew inspection is made - new queue number and open door");
 		simuContr.newInspection();
 		System.out.println("...");
 	}
-	
 }
