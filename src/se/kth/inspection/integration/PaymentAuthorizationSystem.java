12715f7fcacd87f1fc5cd06c5fbd690d9f08fcb7
William Åkerman <wakerman@kth.se>
2017-05-02 01:22:40 +0200

    Update PaymentAuthorizationSystem.java

--- src/se/kth/inspection/integration/PaymentAuthorizationSystem.java
+++ src/se/kth/inspection/integration/PaymentAuthorizationSystem.java
@@ -1,9 +1,9 @@
 package se.kth.inspection.integration;
 
-import se.kth.inspection.model.CreditCardInformation;
+import se.kth.inspection.util.CreditCardInformation;
 
 /**
- * A system which tell if it's ok to use the credit car to pay
+ * A system which tell if it's ok to use the credit car to pay.
  *
  */
 public class PaymentAuthorizationSystem {
@@ -12,7 +12,7 @@ public class PaymentAuthorizationSystem {
 	 * A request if the credit card is Ok or not.
 	 * 
 	 * @param creditCardInformation The information given about the credit card
-	 * @return A <code>boolenan</code> which is always <code>true</code>
+	 * @return A <code>boolenan</code> which is always <code>true</code>.
 	 */
 	public boolean cardRequest (CreditCardInformation creditCardInformation) {
 		return true;
