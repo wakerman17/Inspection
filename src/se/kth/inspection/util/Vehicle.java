9ead1367fcffe603e0332063c4392ccee562449e
William Åkerman <wakerman@kth.se>
2017-05-02 01:08:57 +0200

    Update Vehicle.java
    More comments

--- src/se/kth/inspection/util/Vehicle.java
+++ src/se/kth/inspection/util/Vehicle.java
@@ -1,18 +1,36 @@
 package se.kth.inspection.util;
 
+/**
+ * Information about the Vehicle
+ *
+ */
 public final class Vehicle {
 	
 	public final String vehiclePrimitive;
 	public Vehicle vehicle;
 	
+    /**
+     * Creates a new instance.
+     *
+     * @param vehiclePrimitive 
+     */
 	public Vehicle(String vehiclePrimitive){
 		this.vehiclePrimitive = vehiclePrimitive;
 	}
-	
+	/**
+	 * Get the vehicle information
+	 * 
+	 * @return The information about the vehicle <code>Object</code>
+	 */
 	public Vehicle getVehicle(){
 		return vehicle;
 	}
 	
+    /**
+     * Get the vehicle information
+     *
+     * @return The vehicle information in a <code>String</code>
+     */
 	public String getVehiclePrimitive(){
 		return vehiclePrimitive;
 	}
@@ -22,9 +40,8 @@ public final class Vehicle {
 	 * 
 	 */
 	public String toString (){
-		StringBuilder vehicleMaker = new StringBuilder();
-		vehicleMaker.append(vehicle);
-		return vehicleMaker.toString();
+		StringBuilder vehicleBuilder = new StringBuilder();
+		vehicleBuilder.append(vehicle);
+		return vehicleBuilder.toString();
 	 }
-	
 }
