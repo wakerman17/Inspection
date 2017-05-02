f58887828c9a00a95ad07dfbfa2472fd36085d11
William Åkerman <wakerman@kth.se>
2017-05-02 13:57:31 +0200

    Update PaymentAuthorizationSystemTest.java

--- Test Packages/se/kth/inspection/integration/PaymentAuthorizationSystemTest.java
+++ Test Packages/se/kth/inspection/integration/PaymentAuthorizationSystemTest.java
@@ -7,13 +7,13 @@ import org.junit.Test;
 import se.kth.inspection.util.CreditCardInformation;
 
 public class PaymentAuthorizationSystemTest {
-	private int pin = 1234;
-	private int number = 5678;
-	private String holder = "William";
-	private String expireDate = "02/20";
-	private int cvc = 123;
-	private CreditCardInformation creditCardInformation = new CreditCardInformation(pin, number, holder, expireDate, cvc);
-	private PaymentAuthorizationSystem paymentAuthorizationSystem = new PaymentAuthorizationSystem();
+	int pin = 1234;
+	int number = 5678;
+	String holder = "William";
+	String expireDate = "02/20";
+	int cvc = 123;
+	CreditCardInformation creditCardInformation = new CreditCardInformation(pin, number, holder, expireDate, cvc);
+	PaymentAuthorizationSystem paymentAuthorizationSystem = new PaymentAuthorizationSystem();
 
 	@Test
 	public void testCardRequest () {
