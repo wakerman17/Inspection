637ae5d717afa39917bb103f8df275354baccf28
William Åkerman <wakerman@kth.se>
2017-05-03 21:51:23 +0200

    Decrease copeling
    Controllers send references to other objects that in turn call other objects. This decrease copeling.

--- src/se/kth/inspection/model/DisplayNext.java
+++ src/se/kth/inspection/model/DisplayNext.java
@@ -4,42 +4,26 @@ import se.kth.inspection.integration.Display;
 import se.kth.inspection.util.QueueNumber;
 
 /**
- * Holding the Queue Number which display may show
+ * Holding the Queue Number which display may show.
  *
  */
 public class DisplayNext {
 
-	private QueueNumber queueNumber;
 	private int nextQueueNumber = 1;
 	private int queueNumberRightNow = 0;
-	
-    /**
-     * Creates a new instance.
-     *
-     * @param queueNumber The interface of queueNumber.
-     * @param display The interface of display.
-     */
-	public DisplayNext (QueueNumber queueNumber, Display display) {
-		this.queueNumber = queueNumber;
-	}
-	
-    /**
-     * Creates a new instance.
-     * 
-     */
-	public DisplayNext () {}
 	
 	/**
 	 * Increase the queue number.
 	 * 
 	 * @param display The interface of display.
+	 * @param queueNumber The number to show to the customer.
 	 */
-	public void increaseQueueNumber (Display display) {
-		increaseQueueNumberPrivate(display);
+	public void increaseQueueNumber (Display display, QueueNumber queueNumber) {
+		increaseQueueNumberPrivate(display, queueNumber);
 	}
 	
 	
-	private void increaseQueueNumberPrivate (Display display) {
+	private void increaseQueueNumberPrivate (Display display, QueueNumber queueNumber) {
 		queueNumberRightNow += nextQueueNumber;
 		queueNumber = new QueueNumber(queueNumberRightNow);
 		display.showQueueNumber(queueNumber);
