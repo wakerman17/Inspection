2ed48af5f297fb58c4603e83701daef06a0cc274
William Åkerman <wakerman@kth.se>
2017-05-15 12:52:18 +0200

    Update QueueNumber.java

--- src/se/kth/inspection/model/QueueNumber.java
+++ src/se/kth/inspection/model/QueueNumber.java
@@ -1,7 +1,5 @@
 package se.kth.inspection.model;
 
-import se.kth.inspection.model.QueueNumber;
-
 /**
  * The number which will stand on the display.
  *
@@ -11,24 +9,24 @@ public class QueueNumber {
 	public int queueNumber;
 
 	/**
-     * Creates a new instance.
-     *
-     * @param queueNumberPrimitive The amount represented by the newly created instance.
-     */
-    public QueueNumber (int queueNumber) {
+	 * Creates a new instance.
+	 *
+	 * @param queueNumberPrimitive The amount represented by the newly created instance.
+	 */
+	public QueueNumber (int queueNumber) {
 		this.queueNumber = queueNumber;
 	}
-    
-    /**
-     * The reference to <code>QueueNumber</code> becomes 0.
-     * 
-     */
+	
+	/**
+	 * The reference to <code>QueueNumber</code> becomes 0.
+	 * 
+	 */
 	public QueueNumber() {
-    	this(0);
-    }
+		this(0);
+	}
 	
 	@Override
-    public String toString () {
+	public String toString () {
 		return Integer.toString(queueNumber);
-    }
+	}
 }
