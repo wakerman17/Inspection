592562fd35a3d84093e981e6ca5c4cfd28bf03e2
William Åkerman <wakerman@kth.se>
2017-05-18 08:37:57 +0200

    Check at seminar 4

--- src/se/kth/inspection/model/ResultObserver.java
+++ src/se/kth/inspection/model/ResultObserver.java
@@ -1,5 +1,9 @@
 package se.kth.inspection.model;
 
+/**
+ * Declares the newResult method used by implementing classes to keep track of number of inspections passed or failed.
+ *
+ */
 public interface ResultObserver {
 	
 	void newResult(String result);
